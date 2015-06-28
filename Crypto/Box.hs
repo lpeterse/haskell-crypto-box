d00871fe7963c96222383b46cd11e31a46f36e15
Lars Petersen <lars@nyantec.com>
2015-06-28 21:49:11 +0200

    Changed naming.

--- Crypto/Box.hs
+++ Crypto/Box.hs
@@ -11,42 +11,38 @@
 --
 -- > {-# LANGUAGE OverloadedStrings #-}
 -- > module Main where
--- >
+-- > 
 -- > import Crypto.Box
 -- > import Crypto.Box.Debug
 -- > import Data.ByteString as B
--- >
--- > -- Choose an implementation (Saltine or similar).
--- > -- Attention: `DebugBoxFactory` does no encryption at all.
--- > type Factory = DebugBoxFactory
--- >
+-- > 
 -- > main :: IO ()
 -- > main = do
 -- >   -- Load the secret key once at the beginning of your program. Don't use it elsewhere!
--- >   factory <- newBoxFactory =<< fromByteString =<< B.readFile "secretkey"
+-- >   keyHolder <- newKeyHolder =<< fromByteString =<< B.readFile "secretkey"
 -- >   -- This is the only point in your program where you define which implemenation to use.
--- >   app (factory :: Factory)
--- >
+-- >   app (keyHolder :: DebugKeyHolder)
+-- > 
 -- > -- The remaining application code does not know about a specific implementation and is therefore
--- > -- limited to the methods defined by the `IsBoxFactory` and `IsBox` interfaces. This makes it easy
+-- > -- limited to the methods defined by the `IsKeyHolder` and `IsSecretary` interfaces. This makes it easy
 -- > -- to change the implementation if necessary. It also protects against certain programming errors.
--- >
--- > app :: IsBoxFactory f => f -> IO ()
--- > app factory = do
--- >   print ("Algorithm used is" ++ show (algorithm factory))
--- >   print ("My own public key is " ++ show (toByteString $ publicKey factory))
+-- > 
+-- > app :: IsKeyHolder k => k -> IO ()
+-- > app keyHolder = do
+-- >   print ("Algorithm used is " ++ show (primitive keyHolder))
+-- >   print ("My own public key is " ++ show (toByteString $ publicKey keyHolder))
 -- >   print ("Enter your friend's public key:")
 -- >   friendsPublicKey <- fromByteString =<< B.getLine
--- >   box <- newBox factory friendsPublicKey
--- >   cipherText <- encrypt box "Hello world!"
--- >   print ("Send this cipher text to your friend:" ++ show cipherText)
--- >   print ("Enter cipher text received from your friend:")
--- >   message <- decrypt box =<< B.getLine
--- >   print ("The message was:" ++ show message)
+-- >   secretary <- newSecretary keyHolder friendsPublicKey
+-- >   cipherText <- encrypt secretary "Hello world!"
+-- >   print ("Send this cipher text to your friend: " ++ show cipherText)
+-- >   print ("Enter cipher text received from your friend: ")
+-- >   message <- decrypt secretary =<< B.getLine
+-- >   print ("The message was: " ++ show message)
 -----------------------------------------------------------------------------
 module Crypto.Box (
-    IsBoxFactory (..)
-  , IsBox (..)
+    IsKeyHolder (..)
+  , IsSecretary (..)
   , IsKey (..)
   ) where
 
@@ -54,51 +50,51 @@ import Data.ByteString
 import Control.Monad.Catch
 import Control.Monad.IO.Class
 
-class (IsBox (Box f), IsKey (PublicKey f), IsKey (SecretKey f)) => IsBoxFactory f where
+class (IsSecretary (Secretary k), IsKey (PublicKey k), IsKey (SecretKey k)) => IsKeyHolder k where
   -- | A `PublicKey` may be shared with others. It's not a secret
   --   and does not need to be protected. It is usually derived from
   --   a `SecretKey`.
-  type (PublicKey f)
+  type (PublicKey k)
   -- | The `SecretKey` is the part that must be kept private.
   --
   -- - If it has a `Show` instance defined it should be a mock
   --   like @SecretKey xxxxxxxxxxxxx@. The risk of unintentionally
   --   exposing it through log files or stack traces is unacceptable.
-  type (SecretKey f)
-  -- | A `Box` contains the state necessary for repeated encryption and decryption
+  type (SecretKey k)
+  -- | A `Secretary` contains the state necessary for repeated encryption and decryption
   --   of messages between you and someone else.
   --
-  -- - The creation of a `Box` is (depending on the algorithm) a quite expensive
-  --   operation. The `Box` should be reused if possible.
-  type (Box f)
-  -- | Create a new `BoxFactory` from a given `SecretKey`.
+  -- - The creation of a `Secretary` is (depending on the algorithm) a quite expensive
+  --   operation. The `Secretary` should be reused if possible.
+  type (Secretary k)
+  -- | Create a new `KeyHolder` from a given `SecretKey`.
   --
   -- - Try to do this once near the beginning of your program and drop
   --   the `SecretKey` afterwards.
-  newBoxFactory       :: MonadIO m => SecretKey f -> m f
-  -- | Create a new `BoxFactory` with a random `SecretKey`.
+  newKeyHolder        :: MonadIO m => SecretKey k -> m k
+  -- | Create a new `KeyHolder` with a random `SecretKey`.
   --
   -- - The `SecretKey` is lost when the program ends as it cannot be
   --   extracted.
-  newRandomBoxFactory :: MonadIO m => m f
-  -- | Create a new `Box` for private communication with someone else identified by `PublicKey`.
-  newBox              :: MonadIO m => f -> PublicKey f -> m (Box f)
+  newRandomKeyHolder  :: MonadIO m => m k
+  -- | Create a new `Secretary` for processing the private communication with someone else identified by `PublicKey`.
+  newSecretary        :: MonadIO m => k -> PublicKey k -> m (Secretary k)
 
   -- | A short description of the algorithm suite, i.e. @curve25519xsalsa20poly1305@.
-  algorithm           :: f -> ByteString
+  primitive           :: k -> ByteString
 
-  -- | Extract __your own__ `PublicKey` from the `BoxFactory`.
-  publicKey           :: f -> PublicKey f
+  -- | Get __your own__ `PublicKey` from the `KeyHolder`.
+  publicKey           :: k -> PublicKey k
 
-class IsBox b where
+class IsSecretary s where
   -- | Encrypt a message.
   --
   -- - The computation is in `IO` as randomness might be required. The result is therefore non-deterministic.
-  encrypt   :: MonadIO m => b -> ByteString -> m ByteString
+  encrypt   :: MonadIO m => s -> ByteString -> m ByteString
   -- | Decrypt and authenticate a message.
   --
   -- - Fails if the message has been tampered with.
-  decrypt   :: MonadThrow m => b -> ByteString -> m ByteString
+  decrypt   :: MonadThrow m => s -> ByteString -> m ByteString
 
 class IsKey k where
   toByteString   :: k -> ByteString
--- Crypto/Box/Debug.hs
+++ Crypto/Box/Debug.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 module Crypto.Box.Debug 
   {-# WARNING "This is a dummy implementation and does no encryption at all. Don't use it in production or with confidential data!" #-}
-  ( DebugBoxFactory (..)
-  , DebugBox        (..)
+  ( DebugKeyHolder  (..)
+  , DebugSecretary    (..)
   , DebugPublicKey  (..)
   , DebugSecretKey  (..)
   ) where
 
 import Data.String
-import qualified Crypto.Box as CB
+import Crypto.Box
 
 data DebugPublicKey
    = DebugPublicKey String
@@ -18,37 +18,37 @@ data DebugSecretKey
    = DebugSecretKey String
    deriving (Eq, Ord, Show)
 
-data DebugBoxFactory
-   = DebugBoxFactory DebugSecretKey
+data DebugKeyHolder
+   = DebugKeyHolder DebugSecretKey
    deriving (Eq, Ord, Show)
 
-data DebugBox
-   = DebugBox DebugBoxFactory DebugPublicKey
+data DebugSecretary
+   = DebugSecretary DebugKeyHolder DebugPublicKey
    deriving (Eq, Ord, Show)
 
-instance CB.IsBoxFactory DebugBoxFactory where
-  type PublicKey  DebugBoxFactory = DebugPublicKey
-  type SecretKey  DebugBoxFactory = DebugSecretKey
-  type Box        DebugBoxFactory = DebugBox
+instance IsKeyHolder DebugKeyHolder where
+  type PublicKey  DebugKeyHolder = DebugPublicKey
+  type SecretKey  DebugKeyHolder = DebugSecretKey
+  type Secretary  DebugKeyHolder = DebugSecretary
 
-  newBoxFactory       = return . DebugBoxFactory
-  newRandomBoxFactory = return $ DebugBoxFactory (DebugSecretKey "4; // chosen by fair dice roll.")
+  newKeyHolder        = return . DebugKeyHolder
+  newRandomKeyHolder  = return $ DebugKeyHolder (DebugSecretKey "4; // chosen by fair dice roll.")
 
-  newBox f k          = return (DebugBox f k)
+  newSecretary f k    = return (DebugSecretary f k)
 
-  algorithm _         = "None! DANGER! FIXME! DONTUSEINPRODUCTION!"
-  publicKey (DebugBoxFactory (DebugSecretKey s)) = DebugPublicKey (reverse s)
+  primitive _         = "Debug! DANGER! FIXME! DONTUSEINPRODUCTION!"
+  publicKey (DebugKeyHolder (DebugSecretKey s)) = DebugPublicKey (reverse s)
 
-instance CB.IsBox DebugBox where
+instance IsSecretary DebugSecretary where
   encrypt b message   = return message
   decrypt b cipher    = return cipher
 
-instance CB.IsKey DebugPublicKey where
+instance IsKey DebugPublicKey where
   toByteString (DebugPublicKey s) = fromString s
   fromByteString = return . DebugPublicKey . show
   length _ = 0
 
-instance CB.IsKey DebugSecretKey where
+instance IsKey DebugSecretKey where
   toByteString (DebugSecretKey s) = fromString s
   fromByteString = return . DebugSecretKey . show
   length _ = 0
